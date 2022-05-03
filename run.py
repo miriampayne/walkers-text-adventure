700fa7dc73ce639fbf6a25ae8898b68f66977da2
Miriam Payne <miriampayne@gmail.com>
2022-05-03 03:58:15 +0000

    call start game function

--- run.py
+++ run.py
@@ -7,9 +7,12 @@ def startGame():
     welcome = pyfiglet.figlet_format("Walkers")
     print (welcome)
     print ("A Zombie Text Adventure by Miriam Payne")
-    print ("Survive the Zombie Apocalypse!!")
+    print ("Mission: Survive the Zombie Apocalypse or Die Trying!")
 
     time.sleep(2)
 
-    name = input("What is your name?")
-    age = int(input("Enter your age?"))
+    name = input("Please confirm your name: ")
+    age = int(input("Please confirm your age: "))
+
+#Step 0: Call main menu procedure
+startGame()
