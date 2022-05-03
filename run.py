bb076f48ac39ec12cd46214d9199db00b7920ecd
Miriam Payne <miriampayne@gmail.com>
2022-05-03 05:12:46 +0000

    add initial action fight or flight

--- run.py
+++ run.py
@@ -51,3 +51,34 @@ def startGame():
 
 #Step 0: Call main menu procedure
 startGame()
+
+#Step 1: Get first action
+action = input("Fight or Flight Response?").upper()
+while action not in ["FIGHT", "RUN"]:
+    time.sleep(1)
+    print ("That is not a valid action")
+    action = input("Fight or Run?").upper()
+
+if action == "FIGHT":
+    if age >= 12:
+        print("Your youthfulness helps you avoid being chomped")
+        time.sleep(1)
+        print ("You made it this time")
+        score = 2
+    else:
+        print ("You attack")
+        time.sleep(1)
+        print ("A combination of old age and slow reactions cause you to be bitten")
+        print ("You're too old to be fighting zombies")
+        score = 1
+elif action == "RUN":
+    if age <=12:
+        print("You run from the zombie, but are too slow")
+        time.sleep(1)
+        print ("This walker clearly has some agility left in them and catches up quick")
+        score = 1
+    else:
+        print ("You run from the zombie")
+        time.sleep(1)
+        print ("Your experienced legs carry you to safety")
+        score = 2
