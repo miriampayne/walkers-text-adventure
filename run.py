dd65d31595e47223336fa927095040e226f17fbf
Miriam Payne <miriampayne@gmail.com>
2022-05-03 08:44:23 +0000

    refactor sleep time with user input

--- run.py
+++ run.py
@@ -12,15 +12,10 @@ def startGame():
     
     welcome = pyfiglet.figlet_format("Walkers")
     print(welcome)
-    print("A Zombie Text Adventure by Miriam Payne")
-    print("Mission: Survive the Zombie Apocalypse or Die Trying!")
-
-    time.sleep(2)
-
+    print("A Zombie Apocalypse Text Adventure by Miriam Payne")
     name = input("Please confirm your name:\n")
     age = int(input("Please confirm your age:\n"))
 
-    time.sleep(1)
     if age > 0 and age < 12:
         print("You're young ... chances of survival are slim ... use size and speed to your advantage")
     elif age >= 12 and age <75:
@@ -30,31 +25,23 @@ def startGame():
 
     # time.sleep(1)
     input("Press Enter To Continue...")
-    print("Try and survive")
 
-    time.sleep(1)
+    print("Mission: Survive or die trying")
     print("Chapter 1")
 
-    time.sleep(1)
-    print("...")
-
-    time.sleep(1)
-
     print("You walk into an abandoned Dublin city center tower block car park")
-    time.sleep(1)
+    input("Press Enter To Continue...")
 
     print("A dodgy vaccine for the pandemic has turned 95% the population into a flesh eating zombies outbreak.")
-    time.sleep(1)
+    input("Press Enter To Continue...")
 
     print("Your sat nav shows you are 8 miles from a safe haven")
-    time.sleep(1)
-
-    time.sleep(1)
+    input("Press Enter To Continue...")
 
     print("")
     print("A zombie horde appears... you grab your hurley from your backpack")
 
-    time.sleep(2)
+    input("Press Enter To Continue...")
 
     score = stage_one()
     score, shotgun = stage_two(score)
@@ -79,31 +66,31 @@ def retry_game():
 def stage_one():
     action = input("(F)ight or (R)un?\n").upper()
     while action not in ["FIGHT", "RUN", "F", "R"]:
-        time.sleep(1)
+        input("Press Enter To Continue...")
         print("That is not a valid action")
         action = input("Fight or Run?\n").upper()
 
     if action == "FIGHT":
         if age >= 12:
             print("Your youthfulness helps you avoid being chomped")
-            time.sleep(1)
+            input("Press Enter To Continue...")
             print("You made it this time")
             score = 2
         else:
             print("You attack")
-            time.sleep(1)
+            input("Press Enter To Continue...")
             print("A combination of old age and slow reactions cause you to be bitten")
             print("You're too old to be fighting zombies")
             score = 1
     elif action == "RUN":
         if age <=12:
             print("You run from the zombie, but are too slow")
-            time.sleep(1)
+            input("Press Enter To Continue...")
             print("This walker clearly has some agility left in them and catches up quick")
             score = 1
         else:
             print("You run from the zombie")
-            time.sleep(1)
+            input("Press Enter To Continue...")
             print("Your athletic legs carry you to safety")
             score = 2
 
@@ -112,49 +99,49 @@ def stage_one():
 # Step 2 only happens if you have survived with a score of 2 now
 def stage_two(score):
     if score == 2:
-        time.sleep(1)
+        input("Press Enter To Continue...")
         print("Chapter 2: ... ") 
-        time.sleep(1)
+        input("Press Enter To Continue...")
 
         print("You come across an abandoned house, an old barn and a boarded-up store")
 
-        time.sleep(1)
+        input("Press Enter To Continue...")
         choice = input("Do you enter the house, barn or the store?\n").upper()
 
         while choice not in["HOUSE", "BARN", "STORE"]:
-            time.sleep(1)
+            input("Press Enter To Continue...")
             print("Sorry. I don't understand")
             choice = input("Do you enter the house or the barn or the store?\n").upper()
 
         if choice == "HOUSE":
-            time.sleep(1)
+            input("Press Enter To Continue...")
             print("You enter the house, rummaging around ... ")
-            time.sleep(1)
+            input("Press Enter To Continue...")
             print("You find a shot gun and three shot gun shells")
-            time.sleep(1)
+            input("Press Enter To Continue...")
             shotgun = True
             score = 3
         elif choice == "STORE":
-            time.sleep(1)
+            input("Press Enter To Continue...")
             print("You enter the store, rummaging around ... ")
-            time.sleep(1)
+            input("Press Enter To Continue...")
             print("You stock up on canned food, drink, first aid and find a gun with 4 bullets")
-            time.sleep(1)
+            input("Press Enter To Continue...")
             gun = True
             score = 3
         elif choice == "BARN":
-            time.sleep(1)
+            input("Press Enter To Continue...")
             print("You enter the barn where you could hear sounds of groaning ... ")
-            time.sleep(1)
+            input("Press Enter To Continue...")
             print("You encounter a mob of zombies")
-            time.sleep(1)
+            input("Press Enter To Continue...")
             print("You didn't survive")
         else:
-            time.sleep(1)
+            input("Press Enter To Continue...")
             print("You enter the store, climbing down the escalater you slip on some blood ... ")
-            time.sleep(1)
+            input("Press Enter To Continue...")
             print("You fall to your death")
-            time.sleep(1)
+            input("Press Enter To Continue...")
             print("You didn't survive")
 
     return (score, shotgun)
@@ -162,18 +149,18 @@ def stage_two(score):
 #Part 3 only happens if you have a score of 3 now
 def stage_three(score, shotgun):
     if score == 3:
-        time.sleep(1)
+        input("Press Enter To Continue...")
         print("Chapter 3: ... ") 
-        time.sleep(1)
+        input("Press Enter To Continue...")
 
         print("You make it to the perimeter of the safe haven")
-        time.sleep(1)
+        input("Press Enter To Continue...")
         print("Only one problem ... ")
-        time.sleep(1)
+        input("Press Enter To Continue...")
         print("Two Zombies")
-        time.sleep(1)
+        input("Press Enter To Continue...")
         print("")
-        time.sleep(1)
+        input("Press Enter To Continue...")
         
         if shotgun:
             print("You have three shots to hit two targets")
@@ -182,18 +169,18 @@ def stage_three(score, shotgun):
             print("You have four shots to hit two targets")
             shots = 4
 
-        time.sleep(1)
+        input("Press Enter To Continue...")
         zombies = 2
         while shots > 0 and zombies > 0:
             hit = randint(1, 3)
 
             if hit in [1, 2]:
-                time.sleep(1)
+                input("Press Enter To Continue...")
                 print("A lucky shot, you killed a zombie")
                 shots = shots - 1
                 zombies = zombies - 1
             else:
-                time.sleep(1)
+                input("Press Enter To Continue...")
                 print("You fire a shot, but miss")
                 shots = shots - 1
 
